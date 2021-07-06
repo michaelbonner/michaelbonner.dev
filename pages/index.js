57542e59911af6404ac075c329af42680e95b22d
Michael Bonner <michaelwbonner@gmail.com>
2021-07-06 10:45:31 -0600

    add other projects

--- pages/index.js
+++ pages/index.js
@@ -7,27 +7,59 @@ import pictureOfMe from "../public/images/on-the-beach.jpg";
 const projects = [
   {
     title: `Primary Children's Hospital: Here Kids Win`,
+    description: `Primary Children's Hospital wanted a website to promote patient stories. Faktory reached out to me to build the site they had envisioned. I'm really happy with how this one came out. Next.js made so much of it really simple.`,
     url: `https://herekidswin.com/`,
     github: `https://github.com/FaktoryUtah/primary-childrens-stories`,
   },
   {
     title: `Wasatch Covers`,
+    description: `An ecommerce site built using Gatsby.js. Load times went from over 10 seconds on their old platform, down to near instantaneous on the new one.`,
     url: `https://wasatchcovers.com/`,
   },
   {
     title: `Ravens Film Works`,
+    description: `Built using Sanity CMS and Next.js. This one turned out really good and loads so fast.`,
     url: `https://ravensfilmworks.com/`,
     github: `https://github.com/michaelbonner/ravens`,
   },
   {
     title: `NEF Programs Administration System`,
+    description: `Big ol' platform to administer all of their programs. Can't show you the code for security reasons, but I'm really proud of it so I wanted to at least put it here. 😆`,
     url: `https://pas.nef1.org/`,
+  },
+];
+const otherThings = [
+  {
+    title: `Podcasts I Listen To`,
+    description: `I am really into podcasts, and people ask what podcasts they should listen to. I built this site to make it easier to share the really good ones.`,
+    url: `https://podcasts.michaelbonner.dev/`,
+    github: `https://github.com/michaelbonner/podcasts-i-listen-to`,
+  },
+  {
+    title: `NHL Arenas To Visit`,
+    description: `The plan is to visit every home ice arena. Seems like a decent way to explore the US and Canada`,
+    url: `https://nhl.michaelbonner.dev/`,
+    github: `https://github.com/michaelbonner/nhl-arenas-react`,
+  },
+  {
+    title: `Days Until`,
+    description: `You ever wanted a site to just tell you how many days until a specific date. Well here you go. Really I just needed something easy to start building Svelte projects.`,
+    url: `https://days-until.vercel.app/`,
+    github: `https://github.com/michaelbonner/days-until`,
+  },
+  {
+    title: `Cookie Parser`,
+    description: `Now you don't have to wonder what all the cookies on a site mean. Just copy and paste the cookies string and the site will tell you what cookies are present with links to learn more.`,
+    url: `https://cookie-parser.vercel.app/`,
+    github: `https://github.com/michaelbonner/cookie-parser`,
   },
 ];
 
 const tools = [
   "JavaScript",
   "TypeScript",
+  "React",
+  "Svelte",
   "Node",
   "PHP",
   "AWS",
@@ -123,18 +155,63 @@ export default function Home() {
           </div>
           <div className="lg:mt-32">
             <h2 className="text-3xl mt-16">Projects I&apos;m proud of</h2>
-            <ul className="mt-8 text-lg lg:grid lg:grid-cols-2 gap-x-4 gap-y-12">
+            <ul className="mt-8 text-lg lg:grid lg:grid-cols-2 gap-x-8 gap-y-12">
               {projects.map((project) => {
                 return (
                   <li className="ml-4 lg:ml-8 mt-8 lg:mt-0" key={project.title}>
-                    <p className="mb-2">
+                    <p>
                       <a
-                        className="inline border-b-2 border-gray-300 hover:text-blue-800 hover:border-blue-400 transition-colors text-xl"
+                        className="inline border-b-2 border-gray-300 hover:text-blue-800 hover:border-blue-400 transition-colors text-2xl"
                         href={project.url}
                       >
                         {project.title}
                       </a>
                     </p>
+                    <p className="mt-2 mb-4">{project.description}</p>
+                    <div className="flex justify-start space-x-4">
+                      {project.url && (
+                        <p>
+                          <a
+                            className="flex space-x-1 border-b-2 border-gray-300 hover:text-blue-800 hover:border-blue-400 transition-colors text-base"
+                            href={project.url}
+                          >
+                            <HiLink className="text-xl" />
+                            <span>View Project</span>
+                          </a>
+                        </p>
+                      )}
+                      {project.github && (
+                        <p>
+                          <a
+                            className="flex space-x-1 border-b-2 border-gray-300 hover:text-blue-800 hover:border-blue-400 transition-colors text-base"
+                            href={project.github}
+                          >
+                            <SiGithub className="text-xl" />
+                            <span>Source</span>
+                          </a>
+                        </p>
+                      )}
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="lg:mt-32">
+            <h2 className="text-3xl mt-16">Things I&apos;ve built for fun</h2>
+            <ul className="mt-8 text-lg lg:grid lg:grid-cols-2 gap-x-8 gap-y-12">
+              {otherThings.map((project) => {
+                return (
+                  <li className="ml-4 lg:ml-8 mt-8 lg:mt-0" key={project.title}>
+                    <p>
+                      <a
+                        className="inline border-b-2 border-gray-300 hover:text-blue-800 hover:border-blue-400 transition-colors text-2xl"
+                        href={project.url}
+                      >
+                        {project.title}
+                      </a>
+                    </p>
+                    <p className="mt-2 mb-4">{project.description}</p>
                     <div className="flex justify-start space-x-4">
                       {project.url && (
                         <p>
