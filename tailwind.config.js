b17bfb11c5641ce685f61612632d2161de0e9b99
Michael Bonner <michaelwbonner@gmail.com>
2022-02-01 22:46:11 -0700

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     fontFamily: {
       sans: ["Graphik", "sans-serif"],
       serif: ["Newsreader", "serif"],
     },
     extend: {},
-  },
-  variants: {
-    extend: {
-      mixBlendMode: ["hover"],
-    },
   },
   plugins: [
     require("@tailwindcss/aspect-ratio"),
