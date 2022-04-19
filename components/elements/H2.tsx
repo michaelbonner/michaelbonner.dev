8cf8b921a59e04c41904bba0cc06e65513cc95af
Michael Bonner <michaelwbonner@gmail.com>
2022-04-18 23:00:24 -0600

    h2 accepts a string

--- components/elements/H2.tsx
+++ components/elements/H2.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react";
+import React from "react";
 
-const H2: FC = ({ children }) => {
+const H2 = ({ children }: { children: string }) => {
   return <h2 className="dark:text-gray-50 text-3xl mt-16">{children}</h2>;
 };
 export default H2;
