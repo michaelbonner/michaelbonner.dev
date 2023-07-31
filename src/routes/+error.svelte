5acb63ca4266d20996a3ca4005df237a463573de
Michael Bonner <michaelwbonner@gmail.com>
2023-07-31 12:59:53 -0600

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex justify-between px-8 container mx-auto py-16">
+	<div class="grid gap-4">
+		<h1 class="text-3xl">{$page.error?.message}</h1>
+		<p>
+			Looks like you've hit a dead end. <a class="underline underline-offset-4" href="/"
+				>Go back home</a
+			>
+		</p>
+	</div>
+</div>
