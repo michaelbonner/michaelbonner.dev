34b019487c8ca8bc8f3f8560f709de14bc9b8451
Michael Bonner <michaelwbonner@gmail.com>
2025-01-29 11:20:13 -0700

    Remove postcss adapter, use adapter vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import tailwindcss from '@tailwindcss/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [enhancedImages(), sveltekit()],
+	plugins: [tailwindcss(), enhancedImages(), sveltekit()],
 	optimizeDeps: {
 		include: ['highlight.js', 'highlight.js/lib/core']
 	}
