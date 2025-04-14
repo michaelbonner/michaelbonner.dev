79247c451e7a1b0f3a59d52663aab955ac128321
Michael Bonner <michaelwbonner@gmail.com>
2025-04-13 19:44:14 -0600

    Add apple touch icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,14 +57,23 @@
 
 <svelte:head>
 	<meta name="theme-color" content="hsl(208, 50%, 30%)" />
-	<link rel="icon" href="/favicon.ico" />
 	<meta property="og:url" content={`https://michaelbonner.dev${page.url.pathname}`} />
 	<meta name="author" content="Michael Bonner" />
 	<meta property="profile:first_name" content="Michael" />
 	<meta property="profile:last_name" content="Bonner" />
 	<meta property="profile:username" content="michaelbonner" />
 	<meta property="fb:app_id" content="383758763158159" />
-	<link rel="apple-touch-icon" href="https://michaelbonner.dev/icon.png" />
+
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
 
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<link
