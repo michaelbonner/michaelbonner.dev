a29cfea0c7ee0d006a6211cc20e3dd3a767deb1d
Michael Bonner <mike@bootpackdigital.com>
2025-05-13 08:36:45 -0600

    Don't show other articles on /blog

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import OtherBlogArticles from '$lib/components/OtherBlogArticles.svelte';
-
-	const slug = (page.route.id ?? '').split('/').pop();
 </script>
 
 <main>
 	<slot />
 
-	<OtherBlogArticles />
+	{#if page.route.id !== '/blog'}
+		<OtherBlogArticles />
+	{/if}
 </main>
