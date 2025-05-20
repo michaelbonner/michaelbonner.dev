ca5e233e0b7c79db24680cff76fe5bb6c849b17d
Michael Bonner <mike@bootpackdigital.com>
2025-05-19 23:51:40 -0600

    Use ts for vite config

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { partytownVite } from '@qwik.dev/partytown/utils';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [tailwindcss(), enhancedImages(), sveltekit(), partytownVite({})]
+});
