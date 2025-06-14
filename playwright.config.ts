63ed2798ab89ff0af3ed7b21dcdbe9ea2785867a
Michael Bonner <mike@bootpackdigital.com>
2025-06-14 13:13:42 -0600

    increase playwright webserver timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
 	webServer: {
 		command: 'bun run build && bun run preview',
-		port: 4173
+		port: 4173,
+		timeout: 2 * 60 * 1000 // 2 minutes
 	},
 	testDir: 'e2e',
 	outputDir: 'playwright-report',
