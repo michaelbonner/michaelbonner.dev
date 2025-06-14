406e98f17ff37ace3a0b3436755730ed438edea0
Michael Bonner <mike@bootpackdigital.com>
2025-06-14 13:19:10 -0600

    Update timeout to 5 mins

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	webServer: {
 		command: 'bun run build && bun run preview',
 		port: 4173,
-		timeout: 2 * 60 * 1000 // 2 minutes
+		timeout: 5 * 60 * 1000 // 5 minutes
 	},
 	testDir: 'e2e',
 	outputDir: 'playwright-report',
