ba58f4519befdc506e9b049272fcab518fd749a1
Michael Bonner <mike@bootpackdigital.com>
2025-06-14 13:04:28 -0600

    add baseurl

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'bun run build && bun run preview',
 		port: 4173
 	},
 	testDir: 'e2e',
-	outputDir: 'playwright-report'
+	outputDir: 'playwright-report',
+	use: { baseURL: 'http://localhost:4173' }
 });
