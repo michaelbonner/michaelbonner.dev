d5d7534a0efe7528f0d4e00309193b1c102f43b3
Michael Bonner <mike@bootpackdigital.com>
2025-08-08 10:17:31 -0600

    add vite-plugin-devtools-json

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [tailwindcss(), enhancedImages(), sveltekit(), partytownVite({})]
+	plugins: [tailwindcss(), enhancedImages(), sveltekit(), partytownVite({}), devtoolsJson()]
 });
