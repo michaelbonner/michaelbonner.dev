2dbf8bc3d9c5b30c836fb639a6f21a0bb8d26f4a
Michael Bonner <mike@bootpackdigital.com>
2025-08-09 20:01:31 -0600

    update transition to be more selective

--- src/styles/classes.ts
+++ src/styles/classes.ts
@@ -2,17 +2,18 @@ import { classNames } from '../functions/classNames';
 
 export const classes = {
 	menuItem: classNames(
-		'text-xl border-b-2 border-gray-400 hover:text-blue-800 hover:border-blue-400 transition-all hover:scale-105 hover:rotate-1',
+		'text-xl border-b-2 border-gray-400 transition-transform',
+		'hover:text-blue-800 hover:border-blue-400 hover:scale-105 hover:rotate-1',
 		'dark:border-gray-500 dark:hover:text-blue-300 dark:hover:border-blue-100'
 	),
 	largeBodyLink: classNames(
-		'inline-flex gap-x-1 items-center text-lg border-b-2 border-gray-400 transition-all',
+		'inline-flex gap-x-1 items-center text-lg border-b-2 border-gray-400 transition-transform',
 		'hover:text-blue-800 hover:border-blue-400 hover:scale-105 hover:rotate-1',
 		'dark:border-gray-500 dark:hover:text-blue-300 dark:hover:border-blue-100'
 	),
 	bodyLink: classNames(
-		'inline-flex border-b border-gray-400 hover:text-blue-800 hover:border-blue-400 transition-all break-words no-underline',
-		'hover:scale-105 hover:rotate-1',
+		'inline-flex border-b border-gray-400 transition-transform break-words no-underline',
+		'hover:text-blue-800 hover:border-blue-400 hover:scale-105 hover:rotate-1',
 		'dark:border-gray-500 dark:hover:text-blue-300 dark:hover:border-blue-100'
 	)
 };
