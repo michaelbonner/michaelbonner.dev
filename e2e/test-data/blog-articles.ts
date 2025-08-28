d69355589d3f7632b117f43c710d7fdfdff36d9b
Michael Bonner <mike@bootpackdigital.com>
2025-08-28 14:19:33 -0600

    Add back e2e/test-data

---
+++ e2e/test-data/blog-articles.ts
@@ -0,0 +1,34 @@
+// Generated file, do not edit directly
+// Test-friendly version of blog articles data
+// This file extracts only the necessary data for testing without SvelteKit-specific imports
+
+export const blogArticlesTestData = [
+	{
+		title: 'Screenshot Maker',
+		slug: 'screenshot-maker'
+	},
+	{
+		title: 'PageSpeed Testing is important, you should take it seriously',
+		slug: 'pagespeed-testing'
+	},
+	{
+		title: 'I made a little app to help me keep track of who has access to what GitHub repository',
+		slug: 'github-repositories-viewer-app'
+	},
+	{
+		title: 'Check out the new Git Branch Name Raycast Extension I made',
+		slug: 'git-branch-name-raycast-extension'
+	},
+	{
+		title: 'I made my first Chrome extension',
+		slug: 'i-made-an-extension'
+	},
+	{
+		title: 'Where I think new web software developers should start in 2022',
+		slug: 'getting-started-as-a-web-developer-in-2022'
+	},
+	{
+		title: 'Set up some aliases',
+		slug: 'set-up-some-aliases'
+	}
+];
