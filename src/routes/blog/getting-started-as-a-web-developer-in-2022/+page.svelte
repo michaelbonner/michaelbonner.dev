83652d35c0ca4750ab1456733922cdc2438ea65a
Michael Bonner <michaelwbonner@gmail.com>
2025-11-27 23:16:17 -0700

    Image optimization

--- src/routes/blog/getting-started-as-a-web-developer-in-2022/+page.svelte
+++ src/routes/blog/getting-started-as-a-web-developer-in-2022/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import michaelbonnerDevGithubScreenshot from '$lib/images/michaelbonner-dev-github-screenshot.png?enhanced';
-	import tuffWebsiteScreenshot from '$lib/images/tuff-website-screenshot.png?enhanced';
+	import michaelbonnerDevGithubScreenshot from '$lib/images/michaelbonner-dev-github-screenshot.webp?enhanced';
+	import tuffWebsiteScreenshot from '$lib/images/tuff-website-screenshot.webp?enhanced';
 	import Seo from '../../../components/Seo.svelte';
 	import { classes } from '../../../styles/classes';
 </script>
--- src/routes/blog/i-made-an-extension/+page.svelte
+++ src/routes/blog/i-made-an-extension/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-	import afterExtension from '$lib/images/after-extension.png?enhanced';
-	import availableInTheChromeWebStore from '$lib/images/available-in-the-chrome-web-store.png?enhanced';
-	import beforeExtension from '$lib/images/before-extension.png?enhanced';
+	import afterExtension from '$lib/images/after-extension.webp?enhanced';
+	import availableInTheChromeWebStore from '$lib/images/available-in-the-chrome-web-store.webp?enhanced';
+	import beforeExtension from '$lib/images/before-extension.webp?enhanced';
 	import chromeBasecampExtension from '$lib/images/chrome-basecamp-ui-extension.jpg?enhanced';
 	import A from '../../../components/A.svelte';
 	import Seo from '../../../components/Seo.svelte';
