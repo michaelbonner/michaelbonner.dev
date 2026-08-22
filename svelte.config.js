690cb0f92b5f24b89f258a0d5b13aab021afce9a
Michael Bonner <mike@bootpackdigital.com>
2026-08-22 13:39:12 -0600

    Use isolated Wrangler config in CI

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,11 @@ import adapter from '@sveltejs/adapter-cloudflare';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			platformProxy: process.env.CI
+				? { configPath: 'wrangler.test.jsonc', persist: false }
+				: undefined
+		})
 	},
 
 	plugins: []
